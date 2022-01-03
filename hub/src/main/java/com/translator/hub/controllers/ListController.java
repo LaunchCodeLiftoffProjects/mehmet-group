0ddcf01bc44c6b92b8dc9f343fb9b5ed918791d0
akgolden1 <akgolden1@users.noreply.github.com>
2022-01-03 17:11:47 -0600

    Added mapping for viewtranslators

--- hub/src/main/java/com/translator/hub/controllers/ListController.java
+++ hub/src/main/java/com/translator/hub/controllers/ListController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/viewtranslators")
+@RequestMapping("translator/viewtranslators")
 public class ListController {
+
+    @Autowired
+    private TranslatorRepository
 }
 //This is my second attempt at updating the ListControlller - 1/3/22 4:50
--- hub/src/main/java/com/translator/hub/controllers/TranslatorController.java
+++ hub/src/main/java/com/translator/hub/controllers/TranslatorController.java
@@ -148,4 +148,19 @@ public class TranslatorController {
         return "translator/detail";
     }
 
+//trying to add the correct mapping to iew translators - 1/3/22_AGB
+    @GetMapping("translator/{viewtranslators}")//path parameter translatorId. This piece of data customizes the response
+    public String displayViewTranslator(Model model, @PathVariable int translatorId) {
+
+        Optional optTranslator = translatorRepository.findById(translatorId);
+        if (optTranslator.isPresent()) {
+            Translator translator = (Translator) optTranslator.get();
+            model.addAttribute("translator", translator);
+
+            return "translator/viewtranslator";
+        } else {
+            return "redirect:../";
+        }
+    }
+
 }
