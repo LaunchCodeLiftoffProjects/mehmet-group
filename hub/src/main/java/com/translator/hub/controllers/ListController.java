2947eedfedbad76b3cf2fe901028b639cfb19ec6
handfulofkeys <lindseylasswell@gmail.com>
2022-01-03 21:27:19 -0600

    Editing ListController

--- hub/src/main/java/com/translator/hub/controllers/ListController.java
+++ hub/src/main/java/com/translator/hub/controllers/ListController.java
@@ -1,9 +1,12 @@
 package com.translator.hub.controllers;
 
+import com.translator.hub.data.LangRepository;
 import com.translator.hub.data.TranslatorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
 
 @Controller
 @RequestMapping(value = "List")
@@ -11,5 +14,16 @@ public class ListController {
 
     @Autowired
     private TranslatorRepository translatorRepository;
+
+    @Autowired
+    private LangRepository languageRepository;
+
+    static HashMap<String, String> columnChoices = new HashMap<>();
+
+    public ListController () {
+        columnChoices.put("all", "All");
+        columnChoices.put("translator", "Translator");
+
+    }
 }
 //This is my second attempt at updating the ListControlller - 1/3/22 4:50
