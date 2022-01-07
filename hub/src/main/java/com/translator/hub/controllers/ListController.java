6aad86a1c62e47e196dfdf49626b4ac2695c66f7
akgolden1 <akgolden1@users.noreply.github.com>
2022-01-06 21:18:35 -0600

    got translator list to work

--- hub/src/main/java/com/translator/hub/controllers/ListController.java
+++ hub/src/main/java/com/translator/hub/controllers/ListController.java
@@ -5,37 +5,36 @@ import com.translator.hub.data.TranslatorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
 
+
+
 @Controller
-
-//commented this out to try pointing to a different html page
-//@RequestMapping(value = "List")
-
-@RequestMapping(value = "translator/viewtranslators")
 public class ListController {
 
     @Autowired
     private TranslatorRepository translatorRepository;
 
-    @Autowired
-    private LangRepository languageRepository;
+/*    @Autowired
+    private LangRepository languageRepository;*/
 
+/*
     static HashMap<String, String> columnChoices = new HashMap<>();
 
     public ListController () {
         columnChoices.put("all", "All");
         columnChoices.put("translator", "Translator");
         columnChoices.put("language", "Language");
-    }
+    }*/
 
-    @RequestMapping("")
-    public String list(Model model) {
+    @GetMapping(value="/viewtranslators")
+    public String findAllTranslators(Model model) {
         model.addAttribute("translators", translatorRepository.findAll());
-        model.addAttribute("language", languageRepository.findAll());
-        return "list";
+      //  model.addAttribute("language", languageRepository.findAll());
+        return "translator/viewtranslators";
     }
 
 }
